c2e61b47d3c54b8b1c1287adc16e1018a602109f
vinodf2f <41716058+vinodf2f@users.noreply.github.com>
2018-12-12 11:33:02 +0530

    added extra fullscreen and exitfullscreen than wesbos assignment

---
+++ script.js
@@ -0,0 +1,96 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const fullScreen = player.querySelector('.full');
+
+
+function togglePlay() 
+{
+	if(video.paused)
+		{video.play();}
+	else 
+		{video.pause();}
+}
+
+function updateButton()
+{
+	const icon = this.paused ? '►' : '❚ ❚';
+	toggle.textContent = icon;
+}
+
+function skip()
+{
+	console.log(this.dataset.skip);
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate()
+{
+	video[this.name] = this.value;
+	console.log(this.value);
+}
+
+function handleProgress()
+{
+	const percent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e)
+{
+	const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+	video.currentTime = scrubTime;
+}
+
+function handleFull()
+{
+	console.log('cnsdn');
+	video.webkitEnterFullScreen();
+
+}
+
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mousedown', () => mousedown = false);
+
+let isFull = false;
+function hh()
+{
+	if(!isFull)
+	{
+		fullScreen.textContent = 'small';
+		player.webkitRequestFullScreen();
+		isFull = true;
+	}
+
+	else
+	{
+		fullScreen.textContent = 'full';
+		document.webkitExitFullscreen();
+		isFull = false;
+	
+	}
+	
+	
+	
+}
+fullScreen.addEventListener('click', hh);
